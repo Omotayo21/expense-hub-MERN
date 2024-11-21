dbe4a03a6384941748688fb0a8143c27fb51c714
Abdulrahman <110260275+Omotayo21@users.noreply.github.com>
2024-11-21 20:11:19 +0100

    Update config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
 // src/config.js
-const BaseUrl = "http://127.0.0.1:3000"; // Localhost during development
-
+//const BaseUrl = "http://127.0.0.1:3000"; // Localhost during development
+const BaseUrl = "https://expense-hub-mern-vfi5.vercel.app/";
 export default BaseUrl;

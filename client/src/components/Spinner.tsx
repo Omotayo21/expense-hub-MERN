cf9ad1b7645974e17239a0fcf7438bf7d7c9fa8d
Abdulrahman <110260275+Omotayo21@users.noreply.github.com>
2024-11-22 01:09:20 +0100

    Update Spinner.tsx

--- client/src/components/Spinner.tsx
+++ client/src/components/Spinner.tsx
@@ -5,7 +5,7 @@ import rolling from "../assets/rolling.svg";
 
 const Spinner = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center">
       <img src={rolling} className="text-blue-700" alt="spinner"  />
     </div>
   );
